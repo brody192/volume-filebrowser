f7747587f2ec9a76e437e59a648756fe370ab3e5
Brody Over <brodyover@gmail.com>
2023-07-25 19:54:33 -0400

    cleanup

--- start.sh
+++ start.sh
@@ -5,20 +5,9 @@ if [ -n $RAILWAY_VOLUME_MOUNT_PATH ]; then
 else
   echo no volume attached, please attach a volume.
   echo upon attaching a volume let this service rebuild
-  echo and then login credentials will be available.
+  echo and then the login credentials will be available.
   exit 0
 fi
-
-# if test -n "${-}"; then
-#   echo volume mount point is set to $RAILWAY_VOLUME_MOUNT_PATH
-# else
-#   echo no volume attached, please attach a volume.
-#   echo upon attaching a volume let this service rebuild
-#   echo and then login credentials will be available.
-#   exit 0
-# fi
-
-echo $FILEBROWSER_USRNAME
 
 FILEBROWSER_USRNAME=$(pwgen -s -0 -a 12 1)
 FILEBROWSER_PSSWD=$(pwgen -s -a 24 1)
