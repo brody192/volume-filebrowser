8a42f30467e03653ea932fc0c33f4b079ba4b906
Brody <brodyover@gmail.com>
2023-12-09 22:19:22 -0500

    Updates and housekeeping

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,11 @@
-FROM alpine:3.18.3 as base
+FROM caddy:latest
 
-FROM base as download
-
-ARG CADDY_VERSION=2.7.4
-
-RUN apk add --no-cache pwgen curl
-
-WORKDIR /download
-
-RUN curl "https://github.com/caddyserver/caddy/releases/download/v${CADDY_VERSION}/caddy_${CADDY_VERSION}_linux_amd64.tar.gz" --location --silent --show-error --output caddy.tar.gz \
-    && tar -zxvf caddy.tar.gz caddy \
-    && chmod +x caddy 
-
-FROM base as run
-
-COPY --from=download /usr/bin/pwgen /usr/bin/pwgen
-COPY --from=download /download/caddy /usr/bin/caddy
+RUN apk add --no-cache pwgen
 
 WORKDIR /app
 
 COPY template.caddyfile ./
 
-COPY start.sh ./
-
-RUN chmod +x start.sh
+COPY --chmod=755 start.sh ./
 
 CMD ["sh", "start.sh"]
--- start.sh
+++ start.sh
@@ -11,8 +11,17 @@ else
   exit 0
 fi
 
-FILEBROWSER_USRNAME=$(pwgen -s -0 -a 12 1)
-FILEBROWSER_PSSWD=$(pwgen -s -a 24 1)
+if [ -z "$FILEBROWSER_USRNAME" ]; then
+  echo no username provided, generating a username that will only be valid for this deployment
+  FILEBROWSER_USRNAME=$(pwgen -s -0 -a 12 1)
+  echo username: $FILEBROWSER_USRNAME
+fi
+
+if [ -z "$FILEBROWSER_PSSWD" ]; then
+  echo no password provided, generating a password that will only be valid for this deployment
+  FILEBROWSER_PSSWD=$(pwgen -s -a 24 1)
+  echo password: $FILEBROWSER_PSSWD
+fi
 
 FILEBROWSER_PSSWD_HASH=$(caddy hash-password --plaintext $FILEBROWSER_PSSWD)
 
@@ -20,9 +29,6 @@ cp -f template.caddyfile Caddyfile
 
 sed -i "s~{\$FILEBROWSER_USRNAME}~$FILEBROWSER_USRNAME~g" Caddyfile
 sed -i "s~{\$FILEBROWSER_PSSWD_HASH}~$FILEBROWSER_PSSWD_HASH~g" Caddyfile
-
-echo username: $FILEBROWSER_USRNAME
-echo password: $FILEBROWSER_PSSWD
 
 caddy fmt --overwrite Caddyfile
 
